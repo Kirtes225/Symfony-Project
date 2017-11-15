65fcbbd8cdb4fcb99e51032dc63c234f8ea8610b
Tomasz Bialek <bialektomasz225@gmail.com>
2017-11-15 11:23:40 +0100

    Added voting

--- src/AppBundle/Entity/Comment.php
+++ src/AppBundle/Entity/Comment.php
@@ -8,18 +8,19 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\Common\Collections\Expr\Value;
 use Doctrine\ORM\Mapping as ORM;
 use FOS\CommentBundle\Entity\Comment as BaseComment;
 
-use FOS\CommentBundle\Model\RawCommentInterface;
 use FOS\CommentBundle\Model\SignedCommentInterface;
+use FOS\CommentBundle\Model\VotableCommentInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity
  * @ORM\ChangeTrackingPolicy("DEFERRED_EXPLICIT")
  */
-class Comment extends BaseComment implements SignedCommentInterface
+class Comment extends BaseComment implements SignedCommentInterface,VotableCommentInterface
 {
     /**
      * @ORM\Id
@@ -65,4 +66,43 @@ class Comment extends BaseComment implements SignedCommentInterface
         return $this->getAuthor()->getUsername();
     }
 
+    /**
+     * @ORM\Column(type="integer")
+     * @var int
+     */
+    protected $score = 0;
+
+    /**
+     * Sets the score of the comment.
+     *
+     * @param integer $score
+     */
+    public function setScore($score)
+    {
+        $this->score = $score;
+    }
+
+    /**
+     * Returns the current score of the comment.
+     *
+     * @return integer
+     */
+    public function getScore()
+    {
+        return $this->score;
+    }
+
+    /**
+     * Increments the comment score by the provided
+     * value.
+     *
+     * @param integer value
+     *
+     * @return integer The new comment score
+     */
+    public function incrementScore($by = 1)
+    {
+        $this->score += $by;
+        return $this->score;
+    }
 }
