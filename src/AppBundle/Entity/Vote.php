77f1020ff966c0348938a88f1be82a74f312a964
Tomasz Bialek <bialektomasz225@gmail.com>
2017-11-15 11:20:52 +0100

    Added voting

--- src/AppBundle/Entity/Vote.php
+++ src/AppBundle/Entity/Vote.php
@@ -8,8 +8,57 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use FOS\CommentBundle\Entity\Vote as BaseVote;
+use FOS\CommentBundle\Model\SignedVoteInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
-class Vote
+/**
+ * @ORM\Entity
+ * @ORM\ChangeTrackingPolicy("DEFERRED_EXPLICIT")
+ */
+class Vote extends BaseVote implements SignedVoteInterface
 {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
 
+    /**
+     * Comment of this vote
+     *
+     * @var Comment
+     * @ORM\ManyToOne(targetEntity=AppBundle\Entity\Comment")
+     */
+    protected $comment;
+
+    /**
+     * Author of the vote
+     *
+     * @ORM\ManyToOne(targetEntity="MyProject\MyBundle\Entity\User")
+     * @var User
+     */
+    protected $voter;
+
+    /**
+     * Sets the owner of the vote
+     *
+     * @param UserInterface $voter
+     */
+    public function setVoter(UserInterface $voter)
+    {
+        $this->voter = $voter;
+    }
+
+    /**
+     * Gets the owner of the vote
+     *
+     * @return UserInterface
+     */
+    public function getVoter()
+    {
+        return $this->voter;
+    }
 }
